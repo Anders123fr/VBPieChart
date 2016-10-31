55f79a97604bf5bb1281664b52770aaf5b93bb8c
Anders Friis <af@andersfr.dk>
2016-10-31 15:50:21 +0100

    Carthage

---
+++ VBPieChart/VBPieChartFramework.h
@@ -0,0 +1,22 @@
+//
+//  VBPieChart.h
+//  VBPieChart
+//
+//  Created by Anders Friis on 31/10/2016.
+//  Copyright © 2016 SAKrisT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VBPieChart.
+FOUNDATION_EXPORT double VBPieChartVersionNumber;
+
+//! Project version string for VBPieChart.
+FOUNDATION_EXPORT const unsigned char VBPieChartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VBPieChart/PublicHeader.h>
+
+#import <VBPieChart/VBPieChart.h>
+#import <VBPieChart/VBPiePiece.h>
+#import <VBPieChart/UIColor+HexColor.h>
+#import <VBPieChart/VBPiePiece_private.h>
